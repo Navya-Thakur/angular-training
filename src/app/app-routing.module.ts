49613f8055e4f9154dee7b0ff694f5c6eaa9a239
navya_laptop <navya.thakur@zeuslearning.com>
2022-11-01 15:20:22 +0530

    path changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/customers' },
-  { path: '**', pathMatch: 'full', redirectTo: '/customers' }
+  { path: 'orders', pathMatch: 'full', redirectTo: '/orders' }
 ];
 
 @NgModule({
